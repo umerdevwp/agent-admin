884bf5d848a47778bc422de5440472c400c0d9c8
Adnan I <adnan.i@allshorestaffing.com>
2019-12-03 20:01:27 +0500

    Missing sidebar.php from last commit added

---
+++ application/views/sidebar.php
@@ -0,0 +1,15 @@
+<!-- SIDE BAR MENU -->
+
+<ul class="rd-navbar-nav">
+<li class="rd-navbar-nav-item">
+  <div class="rd-navbar-title"><span class="rd-navbar-title-icon"><span class="fa-ellipsis-h"></span></span><span class="rd-navbar-title-text">Menu</span></div>
+</li>
+<li class="rd-navbar-nav-item"><a class="rd-navbar-link" href="portal"><span class="rd-navbar-icon fa-home"></span><span class="rd-navbar-link-text">Dashboard</span></a> </li>
+<li class="rd-navbar-nav-item">
+  <div class="rd-navbar-title"><span class="rd-navbar-title-icon"><span class="fa-ellipsis-h"></span></span><span class="rd-navbar-title-text">company profile</span></div>
+</li>
+<li class="rd-navbar-nav-item"><a class="rd-navbar-link" href="#"><span class="rd-navbar-icon fa-calendar"></span><span class="rd-navbar-link-text">Entity</span></a> 
+  <ul class="rd-navbar-dropdown">
+    <li class="rd-navbar-dropdown-item"><a class="rd-navbar-link" href="entity/form"><span class="rd-navbar-icon fa-user"></span>Add Entity</a> </li>
+  </ul>
+</li>
