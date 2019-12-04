8132f3ee6a070ea1e8f4341da5cdb437375b3947
Najm ul Abideen <najm.a@allshorestaffing.com>
2019-12-04 23:39:32 +0500

    Adding missing files of last commits

---
+++ application/controllers/Attachments.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+use Src\Services\OktaApiService as Okta;
+
+class Attachments extends CI_Controller
+{
+    public function index()
+    {
+        if(!isSessionValid("Attachments")) redirectSession();
+
+        $this->load->model("attachments_model");
+        $this->load->model("accounts_model");
+        
+        $id = $this->session->user['zohoId'];
+
+        // fetch all childrens ids, to later fetch
+        $result = $this->accounts_model->loadChildAccounts($id,"id");
+
+        // create comma seprated ids from result
+        $arCommaIds = array();
+        foreach($result as $v)
+        {
+            $arCommaIds[] = $v->id;
+        }
+        // add parent id as well
+        $arCommaIds[] = $id;
+
+        $data['attachments'] = $this->attachments_model->getAllFromEntityList($arCommaIds);
+        
+        $this->load->view("header");
+        $this->load->view("attachments",$data);
+        $this->load->view("footer");
+    }
+}
