604ad3a875ccaccfd3f475e39d5a0285a5566f44
Adnaneddie <adnan.i@allshorestaffing.com>
2020-01-20 20:39:57 +0500

    AG-44 done, Changes in entity page and add entity page

--- application/views/entity-add.php
+++ application/views/entity-add.php
@@ -314,7 +314,19 @@
   </div>
 </div>
 
-
+<!-- Modal for popup box -->
+<div class="modal_wrapper" id="modalpopup">
+	<div class="popup_modal_body">
+      <div class="popup_body">
+        Are you sure to discard the data?
+      </div>
+      <div class="btn_area">
+        <input type="submit" class="btn btn-secondary" value="Cancel" id="cancel_Btn">
+        <input type="submit" class="btn btn-success" value="Ok" id="okBtn">
+      </div>
+	</div>
+</div> 
+</section>
 
 </section>
 
@@ -325,16 +337,7 @@
        $('#inputNotificationContactType').val('<?php print !empty($this->input->post("inputNotificationContactType")) ? $this->input->post("inputNotificationContactType") : '-Select-' ?>');
        $('#inputNotificationState').val('<?php print !empty($this->input->post("inputNotificationState")) ? $this->input->post("inputNotificationState") : '-Select-' ?>');
     });
-    jQuery('input#cancelBtn').on('click', function($){
-      if(confirm("Are you sure to discard the data?")){
-        //alert("this is portal page");
-        location.replace("portal");
-      }else{
-        return false;
-      }
-
     });
-    
 </script>
 <script>
 
