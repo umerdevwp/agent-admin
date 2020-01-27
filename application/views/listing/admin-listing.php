1d5bfedf351fb817747e51908c82304dac2c911f
Omer Shafqat <omer.s@datum.pk>
2020-01-28 00:09:13 +0500

    Improvements has been made to the toggle function, promise function is added which is trimming of the error class

--- application/views/listing/admin-listing.php
+++ application/views/listing/admin-listing.php
@@ -1,10 +1,16 @@
 <?php if (!empty($admins)) : ?>
     <?php foreach ($admins as $admin) : ?>
         <tr id="<?= $admin->id; ?>" role="row" class="odd">
-            <td><?= $admin->first_name; ?></td>
-            <td><?= $admin->last_name; ?></td>
+            <td data-name="first_name" class="editable"><?= $admin->first_name; ?></td>
+            <td data-name="last_name" class="editable"><?= $admin->last_name; ?></td>
             <td><?= $admin->email; ?></td>
             <td><?= $admin->last_logged_time; ?></td>
+            <td>
+                <button style="display: none" class="update_<?= $admin->id; ?>" onclick="submitHandler('<?= $admin->id; ?>');">Update</button>
+                <button class="edit_<?= $admin->id; ?>" onclick="updateHandler('<?= $admin->id; ?>');">Edit</button>
+                <button style="display: none" class="reset_<?= $admin->id; ?>" onclick="resetHandler('<?= $admin->id; ?>');">Reset</button>
+                <button onclick="deleteHandler('<?= $admin->id; ?>')">Delete</button>
+            </td>
         </tr>
     <?php endforeach; ?>
 <?php endif; ?>
