3cd72a2575234a710c2179e0dffb5bea878ff69d
Omer Shafqat <omer.s@datum.pk>
2020-07-07 21:03:15 +0500

    Added the tree table for displaying child entities

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32375fe8aed5682c40674794d758f274.js"
+  "/precache-manifest.ef47226db83cde59d1162abe07b2e64c.js"
 );
 
 self.addEventListener('message', (event) => {
