b06c055d08c3482ecb2e72787756242f155f77b6
Omer Shafqat <omer.s@datum.pk>
2020-12-01 22:15:41 +0500

    Removed date formatting from the dashboard table

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.384c625c9a9e30883ac4cdb628e050d4.js"
+  "/precache-manifest.32bfaf08aa1108e069b0db21db73d967.js"
 );
 
 self.addEventListener('message', (event) => {
